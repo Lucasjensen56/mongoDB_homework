4bd1e4b4b2b7ae97b563ac2476a6d7008a71da54
Lucas Jensen <lucasjensen56@gmail.com>
2018-06-15 12:51:35 -0500

    all routes functioning correclty

--- server.js
+++ server.js
@@ -31,17 +31,72 @@ mongoose.connect("mongodb://localhost/articleScraper");
 // when server starts - creates and save a new user document
 
 // routes
+
+// get the scraping results
 app.get("/scrape", function(req, res) {
-	axios.get("https://www.bbc.com/news").then(function(response) {
+	axios.get("http://www.startribune.com/").then(function(response) {
 		var $ = cheerio.load(response.data);
 
-		$("h3").each(function(i, element) {
+		$("div.tease").each(function(i, element) {
 
-			var title = $(element).text();
-			console.log(title)
-		})
+			
+			var result = {};
+
+			result.title = $(this)
+			.children("h3")
+			.text();
+			result.link = $(this)
+			.children("a")
+			.attr("href");
+			result.summary = $(this)
+			.children("div.tease-summary")
+			.text();
+			result.whenPublished = $(this)
+			.children("div.tease-timestamp.js-timestamp ")
+			.text()
+
+			// console.log(result)
+
+			db.Article.create(result)
+			.then(function(dbArticle) {
+				console.log(dbArticle)
+			})
+			.catch(function(err) {
+				return res.json(err);
+			})
+		});
+		res.send("scrape complete")
+	});
+});
+
+// route to grab aritcles from database
+app.get("/articles", function(req, res) {
+	db.Article.find({})
+	.then(function(dbArticle) {
+		res.json(dbArticle);
 	})
-})
+	.catch(function(err) {
+		res.json(err)
+	});
+});
+
+// route to grab aritcle and populte it with its note
+
+app.get("/articles/:id", function(req, res) {
+	db.Note.create(req.body)
+	.then(function(dbNote) {
+		return db.Article.findOneAndUpdate({ _id: req.params.id}, { note: dbNote._id }, { new: true });
+	})
+	.then(function(dbArticle) {
+		res.json(dbArticle);
+	})
+	.catch(function(err) {
+		res.json(err)
+	});
+});
+
+
+
 
 app.listen(PORT, function() {
 	console.log("App running on port " + PORT + "!");
