29fcb6df0d2811446d104b5ca392572a2796eebf
Lucas Jensen <lucasjensen56@gmail.com>
2018-06-16 12:52:41 -0500

    added data.id to notes section

--- public/app.js
+++ public/app.js
@@ -6,8 +6,33 @@ $.getJSON("/articles", function(data) {
 		$("#articles").append("<h3> " + data[i].title + "</h3>")
 		$("#articles").append("<p>" + data[i].summary + "</p>")
 		$("#articles").append("<a href='" + data[i].link + "'>" + "link to Article" + "</a>")
-		$("#articles").append("<div class='form-group'>" + "<label class='form-label' for='note-input'>" + "Add a Note" + "</label>" + "<textarea class='form-input' id='note-input' rows='3'></textarea>" + "</div>")
-		$("#articles").append("<button class='btn'>" + "Add Note" + "</button>")
+		$("#articles").append("<div data-id='" + data[i]._id + "' class='form-group'>" + "<label class='form-label' for='note-input'>" + "Add a Note" + "</label>" + "<textarea class='form-input' id='note-input' rows='3'></textarea>" + "</div>")
+		$("#articles").append("<button id='noteButton' class='btn'>" + "Add Note" + "</button>")
 	}
-});													
+});		
 
+
+// $(document).ready(function() {
+  
+// });
+
+
+$(document).on("click", "#noteButton", function() {
+
+
+	var thisId = $(this).attr("data-id");
+
+	$.ajax({
+		method: "POST",
+		url: "/articles/" + thisId,
+		data: {
+			body: $("#note-input").val()
+		}
+	})
+	.then(function(data) {
+		console.log(data);
+		$("#note-input").empty();
+	})
+
+
+})
